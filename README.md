68913ba8579f73b71d7f88ee7a460875ebe1b53b
Shaeen Singh <shaeendev@gmail.com>
2023-10-09 21:04:11 +0200

    feature(js challenge): added my first js coding interview challenge

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# interview-questions
+# Awesome Interview Coding Challenge [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/scriptjumper/interview-questions/settings)
+
+### Table of Contents
+
+- [Programming Languages/Frameworks/Platforms](#programming-languagesframeworksplatforms)
+  - [JavaScript](#javascript)
+
+## Programming Languages/Frameworks/Platforms
+
+### JavaScript
+
+* [Challenge #1](./javascript/coding-interview-question-1.js)
---
+++ javascript/coding-interview-question-1.js
@@ -0,0 +1,16 @@
+// Coding Interview Question #1
+
+/**
+ * Write a JavaScript function, `solution`, that takes a string as its input and returns the reversed version of the input string. 
+ * You should not use any built-in JavaScript functions or methods such as `reverse()`, `split()`, or `join()`.
+ */
+
+const str = "hello";
+
+function solution(str) {
+    // Your code here
+}
+
+console.log(solution("hello")); // Output: "olleh"
+console.log(solution("world")); // Output: "dlrow"
+console.log(solution("abc123")); // Output: "321cba"
